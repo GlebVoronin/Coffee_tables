35d83f09ff269892b12d2f8ad1093232449ed2e6
GlebVoronin <GlebVoronin03@yandex.ru>
2019-11-15 02:03:16 +0300

    Таблица данных о кофе

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import sys
-from random import randint
+import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.flag = False
-        self.values = 0, 0, 0
-        self.pushButton.clicked.connect(self.flag_activate)
+        self.database_way = 'coffee.sqlite'
+        uic.loadUi('main.ui', self)
+        data = self.get_data()
+        self.tableWidget.setRowCount(len(data))
+        self.tableWidget.setColumnCount(7)
+        self.titles = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах',
+                       'описание вкуса', 'цена', 'объем упаковки']
+        for i, elem in enumerate(data):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
 
-    def draw_yellow_circles(self):
-        self.qp.setBrush(QColor(255, 255, 0))
-        if self.flag:
-            x, y, size = randint(40, 760), randint(40, 560), randint(3, 60)
-            self.values = x, y, size
-        self.qp.drawEllipse(self.values[0], self.values[1], self.values[2], self.values[2])
-
-    def flag_activate(self):
-        self.flag = True
-
-    def paintEvent(self, event):
-        self.qp = QPainter()
-        self.qp.begin(self)
-        self.draw_yellow_circles()
-        self.qp.end()
-        self.flag = False
-        self.update()
+    def get_data(self):
+        database = sqlite3.connect(self.database_way)
+        cursor = database.cursor()
+        new_data = []
+        data = cursor.execute("""SELECT id, title, degree_of_roasting,
+        type, description, price, volume FROM Coffee""").fetchall()
+        for element in data:
+            cursor = database.cursor()
+            degree = cursor.execute(f"""SELECT title FROM Degree_of_roasting
+            WHERE id = {element[2]}""").fetchone()[0]
+            cursor = database.cursor()
+            type_of_coffee = cursor.execute(f"""SELECT title FROM Types_of_coffee
+            WHERE id = {element[3]}""").fetchone()[0]
+            new_element = [element[0], element[1], degree, type_of_coffee,
+                           element[4], element[5], element[6]]
+            new_data.append(new_element)
+        return new_data
 
 
 app = QApplication(sys.argv)
